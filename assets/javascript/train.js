0e54e4bbf3881979262b5366641df8b4242b0550
Layla Jooya <laylajooya@gmail.com>
2019-04-16 09:19:22 -0500

    working train schedule

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -10,6 +10,14 @@ var config = {
   
 firebase.initializeApp(config);
 
+var clearInputs = function() {
+    $("#train").val("");
+    $("#city").val("");
+    $("#first-leaves").val("");
+    $("#leaves-every").val("");
+
+};
+
 var createTrain = firebase.database();
 
 $("#add-button").on("click",function() {
@@ -18,18 +26,61 @@ $("#add-button").on("click",function() {
     var city = $("#city").val().trim();
     var firstLeaves = $("#first-leaves").val().trim();
     var leavesEvery = $("#leaves-every").val().trim();
-    var addTrain = {name: train, destination: city, firstTrain: firstLeaves, frequency: leavesEvery};
 
-createTrain.ref().push(addTrain);
+    var addTrain = {
+        name: train, 
+        destination: city, 
+        firstTrain: firstLeaves, 
+        frequency: leavesEvery
+    }
+
+    console.log(addTrain.name);
+    console.log(addTrain.destination);
+    console.log(addTrain.firstTrain);
+    console.log(addTrain.frequency);
+
+
+    createTrain.ref().push(addTrain);
+
+    clearInputs();
+
+    return false;
+
 
 });
 
-createTrain.ref().on("child_added", function(childSnapshot, prevChildKey) {
+createTrain.ref().on("child_added", function(childSnapshot) {
 
     var trainName = childSnapshot.val().name;
     var cityName = childSnapshot.val().destination;
     var trainFirst = childSnapshot.val().firstTrain;
     var trainFreq = childSnapshot.val().frequency;
+
+    console.log(trainName);
+    console.log(cityName);
+    console.log(trainFirst);
+    console.log(trainFreq);
+
+    var firstTimeConverted = moment(trainFirst, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    var currentTime = moment();
+    console.log("Current time: "+ moment(currentTime).format("hh:mm"));
+
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("Difference in time: " + diffTime);
+
+    var tRemainder = diffTime % trainFreq;
+    console.log(tRemainder);
+
+    var tMinutesTillTrain = trainFreq - tRemainder;
+    console.log("Minutes till train: " + tMinutesTillTrain);
+
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("Arrival time: " + moment(nextTrain).format("hh:mm"));
+
+
+    $("#train-sched > tbody").append("<tr> <td>" + trainName + "</td> <td>" + cityName + "</td> <td>" + trainFreq + "</td> <td>" + moment(nextTrain).format("hh:mm") + "</td> <td>" + tMinutesTillTrain + "</td> </tr>");
 
 });
 
