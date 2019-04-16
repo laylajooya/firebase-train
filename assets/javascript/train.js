a92998dd246f99204a818de7dc080fb862caa3e9
Layla Jooya <laylajooya@gmail.com>
2019-04-16 10:07:48 -0500

    add comments to js

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -18,15 +18,19 @@ var clearInputs = function() {
 
 };
 
+// Variable to reference database
 var createTrain = firebase.database();
 
+// Function when add train button clicked
 $("#add-button").on("click",function() {
 
+    // User input
     var train = $("#train").val().trim();
     var city = $("#city").val().trim();
     var firstLeaves = $("#first-leaves").val().trim();
     var leavesEvery = $("#leaves-every").val().trim();
 
+    // Create local object to hold user input
     var addTrain = {
         name: train, 
         destination: city, 
@@ -34,14 +38,15 @@ $("#add-button").on("click",function() {
         frequency: leavesEvery
     }
 
+    // Adds data to Firbase
+    createTrain.ref().push(addTrain);
+
     console.log(addTrain.name);
     console.log(addTrain.destination);
     console.log(addTrain.firstTrain);
     console.log(addTrain.frequency);
 
-
-    createTrain.ref().push(addTrain);
-
+    // Clears input fields
     clearInputs();
 
     return false;
@@ -49,8 +54,10 @@ $("#add-button").on("click",function() {
 
 });
 
+// Creates an event in Firebase
 createTrain.ref().on("child_added", function(childSnapshot) {
 
+    // Event with variables
     var trainName = childSnapshot.val().name;
     var cityName = childSnapshot.val().destination;
     var trainFirst = childSnapshot.val().firstTrain;
@@ -61,6 +68,7 @@ createTrain.ref().on("child_added", function(childSnapshot) {
     console.log(trainFirst);
     console.log(trainFreq);
 
+    // Moment.js y'all
     var firstTimeConverted = moment(trainFirst, "HH:mm").subtract(1, "years");
     console.log(firstTimeConverted);
 
@@ -79,8 +87,8 @@ createTrain.ref().on("child_added", function(childSnapshot) {
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
     console.log("Arrival time: " + moment(nextTrain).format("hh:mm"));
 
-
-    $("#train-sched > tbody").append("<tr> <td>" + trainName + "</td> <td>" + cityName + "</td> <td>" + trainFreq + "</td> <td>" + moment(nextTrain).format("hh:mm") + "</td> <td>" + tMinutesTillTrain + "</td> </tr>");
+// Adds data to table in html
+$("#train-sched > tbody").append("<tr> <td>" + trainName + "</td> <td>" + cityName + "</td> <td>" + trainFreq + "</td> <td>" + moment(nextTrain).format("hh:mm") + "</td> <td>" + tMinutesTillTrain + "</td> </tr>");
 
 });
 
